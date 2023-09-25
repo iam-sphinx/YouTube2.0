f7fe50233f57a9f0e812a54a0206d0803ed54ab1
iam-sphinx <kandarp1803@gmail.com>
2023-09-25 10:28:09 +0530

    router

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import styled, { ThemeProvider } from "styled-components";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 import { darkTheme, lightTheme } from "./utils/Theme";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Video from "./pages/Video";
 import SignIn from "./pages/SignIn";
@@ -27,7 +27,7 @@ function App() {
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <Container>
-        <BrowserRouter>
+        <HashRouter>
           <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
           <Main>
             <Navbar />
@@ -47,7 +47,7 @@ function App() {
               </Routes>
             </Wrapper>
           </Main>
-        </BrowserRouter>
+        </HashRouter>
       </Container>
     </ThemeProvider>
   );
